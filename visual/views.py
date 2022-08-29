e6c8c771e6878c0e9e3b21c664643246bb6ab172
LiuZhengDS <liuzh0110@gmail.com>
2022-08-29 15:26:28 +0800

    20220829 update

--- visual/views.py
+++ visual/views.py
@@ -59,16 +59,22 @@ def read_data(source_path, target_path, file_name, file_name_save):
 
     return df
 
-def get_kpi(df, axis = 0):
+def get_kpi(df, column='ph1_unbalance_hde', axis = 0):
+
+    df = df.loc[:, [column]]
 
     print(df)
+    print(df.mean(axis))
+    try:
+        df_mean = df.mean(axis)[0]
+        df_std = df.std(axis)[0]
+        df_median = df.median(axis)[0]
+    
+    except:
+        df_mean = 'N/A'
+        df_std = 'N/A'
+        df_median = 'N/A'
 
-    df_mean = df.mean(axis)[0]
-    
-    df_std = df.std(axis)[0]
-    
-    df_median = df.median(axis)[0]
-    
     return {
         "df_mean": df_mean,
         "df_std": df_std,
@@ -120,7 +126,7 @@ def search(request, column, kw, df):
     return HttpResponse(json.dumps(res, ensure_ascii=False), content_type="application/json charset=utf-8") # 返回结果必须是json格式
 
 
-def query(request):
+def query(request, source_path=sourth_path, target_path=target_path, file_name=file_name, file_name_save=file_name_save):
     """
     query方法要实现以下后续功能:
 
@@ -129,69 +135,61 @@ def query(request):
     c. Pandas读取数据后, 将前端选择的DIMENSION作为pivot_table方法的column参数
     d. 返回Json格式的结果
     """
-    df = pd.read_csv('C:/Users/1000297658/Desktop/dataset/Return_yeah.csv') #将sql语句结果读取至Pandas Dataframe
-
     form_dict = dict(six.iterlists(request.GET))
-    print(request.GET)
-    print(form_dict)
 
-    dimension_selected = form_dict['df_mean'][0]
-    #  如果字段名有空格为了SQL语句在预设字典中加了中括号的，这里要去除
-    if dimension_selected[0] == '[':
+    print('request.GET:\n', request.GET)
+    print('obtained dictionary:\n', form_dict)
 
-        column = dimension_selected[1:][:-1]
-    else:
-        column = dimension_selected
+    # print('\nget some elements:\n', form_dict['Feature1'][0])
 
-    # KPI
-    kpi = get_kpi(df, column)
-
-    context = {
-        "df_mean": kpi["df_mean"],
-        "df_std": kpi["df_std"],
-        "df_median": kpi["df_median"],
-        'data': df.to_html()
-    }
-
-    return HttpResponse(json.dumps(context, ensure_ascii=False), content_type="application/json charset=utf-8") # 返回结果必须是json格式
-
-
-def index(request, source_path=sourth_path, target_path=target_path, file_name=file_name, file_name_save=file_name_save):
- 
-    mselect_dict = {}
-    form_dict = dict(six.iterlists(request.GET))
-    print(request.GET)
-    print(form_dict)
     # df = pd.read_excel('C:/Users/sas053/Desktop/jmp_feature_importance.xlsx') #将sql语句结果读取至Pandas Dataframe
     df = pd.read_csv('C:/Users/1000297658/Desktop/dataset/Return_yeah.csv') #将sql语句结果读取至Pandas Dataframe
 
     df = read_data(source_path, target_path, file_name, file_name_save)
-    df = df.iloc[0:10]
+    df = df.iloc[0:30]
 
-    print(type(pd.DataFrame(df.iloc[0,1:3])), '\n', df)
-
-    
     kpi = get_kpi(df)
-    
-    dct = columns2dictionary(df)
-    # D_MULTI_SELECT
-    for key, value in dct.items():
-        mselect_dict[key] = {}
-        mselect_dict[key]['select'] = value
-        mselect_dict[key]['options'] = get_distinct_list(df, value) # 以后可以后端通过列表为每个多选控件传递备选项
-    
-    # print(mselect_dict)
+ 
+    if form_dict:
+        column = form_dict['Feature1'][0]
+        kpi = get_kpi(df, column)
+        df = df.loc[:, [column]]
 
-    df = df.iloc[:, 8:9]
+    print(df)
     
     context = {
         "df_mean": kpi["df_mean"],
         "df_std": kpi["df_std"],
         "df_median": kpi["df_median"],
         'data': df.to_html(),
+    }
+    
+    return HttpResponse(json.dumps(context, ensure_ascii=True), content_type="application/json charset=utf-8") # 返回结果必须是json格式
+
+
+def index(request, source_path=sourth_path, target_path=target_path, file_name=file_name, file_name_save=file_name_save):
+ 
+    mselect_dict = {}
+
+    form_dict = dict(six.iterlists(request.GET))
+    print('request.GET:\n', request.GET)
+    print('obtained dictionary:\n', form_dict)
+
+    df = pd.read_csv('C:/Users/1000297658/Desktop/dataset/Return_yeah.csv') #将sql语句结果读取至Pandas Dataframe
+    df = read_data(source_path, target_path, file_name, file_name_save)
+    df = df.iloc[0:50]
+
+    dct = columns2dictionary(df)
+
+    # D_MULTI_SELECT
+    for key, value in dct.items():
+        mselect_dict[key] = {}
+        mselect_dict[key]['select'] = value
+        mselect_dict[key]['options'] = get_distinct_list(df, value) # 以后可以后端通过列表为每个多选控件传递备选项
+
+    context = {
         'mselect_dict': mselect_dict,
     }
     
-
     return render(request, 'visual/display.html', context)
     # return HttpResponse(df.to_html(max_cols=10,show_dimensions=True)) #渲染，这里暂时渲染为最简单的HttpResponse，以后可以扩展
