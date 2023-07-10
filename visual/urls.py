32824d8ece7ae87aea2bf350dc396960aafac6e2
1000300246 <mia.sun@wdc.com>
2023-07-10 14:32:27 +0800

    newcode

--- visual/urls.py
+++ visual/urls.py
@@ -6,9 +6,13 @@ urlpatterns = [
     path('index', views.index, name='index'),
     # path(r'^add_query', views.add_query)
     path(r'query', views.query, name="query"),
+    path(r'query1', views.query1, name="query1"),
     path('blog', views.blog, name = 'blog'),
     path(r'showdata', views.showdata, name="showdata"),
     path(r'plot', views.plot, name="plot"),
+    # path(r'index1', views.IndexView.as_view(), name='visual'),
+    # path(r'^bar', views.ChartView.as_view(), name='visual'),
+
 ]
 
 
